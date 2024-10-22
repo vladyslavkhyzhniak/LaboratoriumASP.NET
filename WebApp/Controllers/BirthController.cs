ea9b5e4f192133fd47e7587b7952200cfd783a1e
vladyslavkhyzhniak <vladyslav.khyzhniak@microsoft.wsei.edu.pl>
2024-10-22 13:12:21 +0200

    Birth

---
+++ WebApp/Controllers/BirthController.cs
@@ -0,0 +1,32 @@
+﻿using LabProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabProject.Controllers;
+
+public class BirthController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Form()
+    {
+        return View();
+    }
+
+    public IActionResult Result(BirthModel birth)
+    {
+        if (!birth.IsValid())
+        {
+            ViewData["Error"] = "Proszę wprowadzić poprawne imię oraz datę urodzenia wcześniejszą niż dzisiaj.";
+            return View("Error");
+        }
+
+        int age = birth.GetAge();
+        ViewBag.Name = birth.Name;
+        ViewBag.Age = age;
+
+        return View();
+    }
+}
---
+++ WebApp/Models/BirthModel.cs
@@ -0,0 +1,24 @@
+﻿namespace LabProject.Models;
+
+public class BirthModel
+{
+    public string Name { get; set; }
+    public DateTime? BirthDate { get; set; }
+
+    public bool IsValid()
+    {
+        return !string.IsNullOrEmpty(Name) && BirthDate.HasValue && BirthDate.Value < DateTime.Now;
+    }
+
+    public int GetAge()
+    {
+        if (BirthDate == null) return 0;
+
+        var today = DateTime.Today;
+        int age = today.Year - BirthDate.Value.Year;
+
+        if (BirthDate.Value.Date > today.AddYears(-age)) age--;
+
+        return age;
+    }
+}
