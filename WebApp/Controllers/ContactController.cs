27a1f6d6cc55e3369fc42bbc735d4d15444af315
vladyslav.khyzhniak <vladyslav.khyzhniak@microsoft.wsei.edu.pl>
2024-10-23 12:10:41 +0200

    lab3 part 1

---
+++ WebApp/Controllers/ContactController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+public class ContactController : Controller
+{
+
+    private static Dictionary<int, ContactModel> _contacts = new()
+    {
+        {1,new ContactModel()
+            {
+            Id  = 1,
+            FirstName = "Adam",
+            LastName = "Babecki",
+            Email = "adam@wsei.edu.pl",
+            PhoneNumber = "111 222 333",
+            Birth = new DateOnly(2001,10,10)
+            } 
+        },
+        {2,new ContactModel()
+            {
+                Id  = 2,
+                FirstName = "Adam2",
+                LastName = "Babecki2",
+                Email = "adam2@wsei.edu.pl",
+                PhoneNumber = "112 222 333",
+                Birth = new DateOnly(2002,12,12)
+            } 
+        },
+        {3,new ContactModel()
+            {
+                Id  = 3,
+                FirstName = "Adam3",
+                LastName = "Babecki3",
+                Email = "adam3@wsei.edu.pl",
+                PhoneNumber = "113 222 333",
+                Birth = new DateOnly(2003,3,13)
+            } 
+        }
+    };
+
+    private static int _currentId = 3;
+    
+    // Lista kontaktów
+    public IActionResult Index()
+    {
+        return View(_contacts);
+    }
+    // Zwraca formularz dodania kontaktu
+    [HttpGet]
+    public IActionResult Add()
+    {
+        return View();
+    }
+    // Odebranie danych z form, zapis kontaktu i powrót do listy
+    [HttpPost]
+    public IActionResult Add(ContactModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View();
+        }
+        // zapisanie dannych
+        model.Id = ++_currentId;
+        _contacts.Add(model.Id, model);
+        return View("Index", _contacts);
+    }
+
+    public IActionResult Delete(int id)
+    {
+        _contacts.Remove(id);
+        return View("Index");
+    }
+
+    public IActionResult Details(int id)
+    {
+        return View("Details");
+    }
+}
---
+++ WebApp/Models/ContactModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Models;
+
+public class ContactModel
+{
+    [HiddenInput]
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Musisz wpisać imię")]
+    [MaxLength(length:20, ErrorMessage = "Imię nie może być dłuższe niż 20 znaków")]
+    [MinLength(length:2, ErrorMessage = "Imię musi mieć co najmniej 2 znaki")]
+    public string FirstName { get; set; }
+    
+    [Required(ErrorMessage = "Musisz wpisać nazwisko")]
+    [MaxLength(length:20, ErrorMessage = "Nazwisko nie może być dłuższe niż 20 znaków")]
+    [MinLength(length:2, ErrorMessage = "Nazwisko musi mieć co najmniej 2 znaki")]
+    public string LastName { get; set; }
+    
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Phone]
+    [RegularExpression("\\d{3} \\d{3} \\d{3}", ErrorMessage = "Wpisz numer wg wzoru: xxx xxx xxx")]
+    public string PhoneNumber { get; set; }
+    
+    [DataType(DataType.Date)]
+    public DateOnly Birth { get; set; }
+    
+    
+}
